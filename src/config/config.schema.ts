f6c68dd927e33fd66e35d4a12c5b679c3e505835
Anton <anton.vidinev@da-14.com>
2020-03-27 01:42:00 +0200

    add simple string validation for env config

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -3,12 +3,15 @@ import { Environment } from '../enums/environment.enum';
 const Joi = require('@hapi/joi');
 
 const portRegex = () => /^([1-9]|[1-5]?[0-9]{2,4}|6[1-4][0-9]{3}|65[1-4][0-9]{2}|655[1-2][0-9]|6553[1-5])$/;
+const dbNameRegex = () => /^[a-zA-Z0-9_]{4,50}$/;
+const hostRegex = () =>
+  /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
 
 export const configSchema = Joi.object({
   PORT: Joi.string().required().trim().regex(portRegex()),
-  DB_NAME: Joi.string().required().trim(),
-  DB_PASS: Joi.string().required().trim(),
-  DB_HOST: Joi.string().required().trim(),
-  DB_USERNAME: Joi.string().required().trim(),
+  DB_NAME: Joi.string().required().trim().min(2).max(64),
+  DB_PASS: Joi.string().required().trim().min(2).max(64),
+  DB_HOST: Joi.string().required().trim().regex(hostRegex()),
+  DB_USERNAME: Joi.string().required().trim().regex(dbNameRegex()),
   ENV: Joi.string().required().lowercase().trim().valid(Environment.Dev, Environment.Prod),
 }).unknown();
