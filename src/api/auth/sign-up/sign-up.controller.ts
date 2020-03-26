1e2fcd154ecfc7b0f83e6af1028e46014405a0d7
Anton <anton.vidinev@da-14.com>
2020-03-27 01:29:21 +0200

    move sign up logic to service

--- src/api/auth/sign-up/sign-up.controller.ts
+++ src/api/auth/sign-up/sign-up.controller.ts
@@ -1,16 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import { User } from '../../../models/user/user.model';
-import { ErrorThrow } from '../../../helpers/custom-errors/throw-error.namespace';
+import { AuthService } from '../auth.service';
 const HttpStatus = require('http-status-codes');
 
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
   const userParams: User = req.body;
   try {
-    if (await User.findOne({ where: { email: userParams.email } })) {
-      return ErrorThrow.conflict(`Email ${userParams.email} is already taken`);
-    }
-
-    await User.create(userParams);
+    await AuthService.signUp(userParams);
     res.sendStatus(HttpStatus.CREATED);
   } catch (error) {
     console.log('Sign Up error');
